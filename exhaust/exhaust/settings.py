9f945fc2709356b70b69e838efe7439a20d61a39
Rūdolfs <gameacc78@inbox.lv>
2021-01-14 10:52:09 +0200

    commit

--- exhaust/exhaust/settings.py
+++ exhaust/exhaust/settings.py
@@ -31,13 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'menu.apps.MenuConfig',
+    'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'menu',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'home'
--- exhaust/menu/views.py
+++ exhaust/menu/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+def swipe(request):
+	return render(request,'menu/swipe.html')
+
+def home (request):
+	return render(request,'menu/home.html')
 # Create your views here.
